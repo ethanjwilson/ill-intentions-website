b36a5e302bb5d806eef751ac92aa8e3580f96cf4
Ethan Wilson <ethanwilson5669@gmail.com>
2021-05-02 23:21:03 +1200

    webhook test 7

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import Cors from "cors";
+import { buffer } from "micro";
 // Initializing the cors middleware
 const cors = Cors({
   methods: ["POST", "HEAD"],
@@ -24,35 +25,45 @@ const endpointSecret = "whsec_9aQrl1xlIYPNzMQgbmJjfT5mp3dVVOc7";
 export default async (req, res) => {
   await runMiddleware(req, res, cors);
 
-  if (endpointSecret) {
-    // Get the signature sent by Stripe
-    try {
-      const signature = req.headers["stripe-signature"];
-      const event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
-      switch (event.type) {
-        case "payment_intent.succeeded":
-          const paymentIntent = event.data.object;
-          console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-          // Then define and call a method to handle the successful payment intent.
-          // handlePaymentIntentSucceeded(paymentIntent);
-          break;
-        case "payment_method.attached":
-          const paymentMethod = event.data.object;
-          // Then define and call a method to handle the successful attachment of a PaymentMethod.
-          // handlePaymentMethodAttached(paymentMethod);
-          break;
-        default:
-          // Unexpected event type
-          console.log(`Unhandled event type ${event.type}.`);
+  if (req.method === "POST") {
+    if (endpointSecret) {
+      try {
+        const buf = await buffer(req);
+        const signature = req.headers["stripe-signature"];
+        const event = stripe.webhooks.constructEvent(buf, signature, endpointSecret);
+        switch (event.type) {
+          case "payment_intent.succeeded":
+            const paymentIntent = event.data.object;
+            console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+            // Then define and call a method to handle the successful payment intent.
+            // handlePaymentIntentSucceeded(paymentIntent);
+            break;
+          case "payment_method.attached":
+            const paymentMethod = event.data.object;
+            // Then define and call a method to handle the successful attachment of a PaymentMethod.
+            // handlePaymentMethodAttached(paymentMethod);
+            break;
+          default:
+            // Unexpected event type
+            console.log(`Unhandled event type ${event.type}.`);
+        }
+        return res.status(200);
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return res.status(400);
       }
-      return res.status(200);
-    } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`, err.message);
-      return res.status(400);
     }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
-
   // await db.collection("payment-intents").doc(checkoutId).update({
   //   complete: true,
   // });
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
